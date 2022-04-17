e85698db986a6f35ebe858051d3cd4706fa4117d
venkat <v.kannan@infocerv.com>
2022-04-17 12:21:31 +0530

    setup recyclerview

--- app/src/main/java/com/venkat/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/venkat/cryptoapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.venkat.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.venkat.cryptoapp.adapter.CurrencyRecyclerviewAdapter
+import com.venkat.cryptoapp.model.CryptoCurrencyItem
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter : CurrencyRecyclerviewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecyclerView()
     }
+
+    private fun initRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = CurrencyRecyclerviewAdapter{currency:CryptoCurrencyItem ->
+            currencyItemClicked(currency)
+        }
+
+    }
+
+    private fun currencyItemClicked(currency : CryptoCurrencyItem){
+        Log.i("MyTag",currency.symbol)
+    }
+
+
 }
--- app/src/main/java/com/venkat/cryptoapp/adapter/CurrencyRecyclerviewAdapter.kt
+++ app/src/main/java/com/venkat/cryptoapp/adapter/CurrencyRecyclerviewAdapter.kt
@@ -8,8 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.venkat.cryptoapp.R
 import com.venkat.cryptoapp.model.CryptoCurrencyItem
 
-class CurrencyRecyclerviewAdapter(private val currencies: List<CryptoCurrencyItem>) :
+class CurrencyRecyclerviewAdapter(private val clickListener: (CryptoCurrencyItem) -> Unit) :
     RecyclerView.Adapter<CurrencyViewHolder>() {
+    private val currencies = ArrayList<CryptoCurrencyItem>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val listItem = layoutInflater.inflate(R.layout.currency_item_layout, parent, false)
@@ -18,11 +19,16 @@ class CurrencyRecyclerviewAdapter(private val currencies: List<CryptoCurrencyIte
 
     override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
         val currencyItem = currencies[position]
-        holder.bind(currencyItem)
+        holder.bind(currencyItem,clickListener)
     }
 
     override fun getItemCount(): Int {
         return currencies.size
+    }
+    fun setList(currencyList : List<CryptoCurrencyItem>){
+        currencies.clear()
+        currencies.addAll(currencyList)
+        notifyDataSetChanged()
     }
 }
 
@@ -31,7 +37,7 @@ class CurrencyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     val currencyPriceTextView = view.findViewById<TextView>(R.id.current_price_text_view)
     val changePercentageTextView = view.findViewById<TextView>(R.id.price_difference_text_view)
 
-    fun bind(currency: CryptoCurrencyItem) {
+    fun bind(currency: CryptoCurrencyItem,clickListener: (CryptoCurrencyItem) -> Unit) {
         val currencyName = "${currency.baseAsset.uppercase()} / ${currency.quoteAsset.uppercase()}"
         currencyNameTextView.text = currencyName
         val currencyPrice = "&#8377;${currency.openPrice}"
@@ -39,5 +45,7 @@ class CurrencyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val changedAmount = currency.openPrice.toDouble().minus(currency.lastPrice.toDouble())
         val changedPercentage = (changedAmount * 100) / currency.lastPrice.toDouble()
         val changePercentageString = "&#9652; $changedPercentage%"
+
+        clickListener(currency)
     }
 }
